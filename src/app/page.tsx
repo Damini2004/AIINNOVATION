5e4f2bd87190d2a7adfb70941b410ac0bea8a73b
Divya Chachane <divyachachane06@gmail.com>
2025-09-18 06:53:06 +0000

    remove this  (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,6 @@ function Showcase() {
       <EventsSection />
       <ContactSection />
       <QuickButtonArea />
-      <MembersPage />
       <main className="flex-1 px-4 py-8 md:px-8 md:py-12">
         
       </main>
