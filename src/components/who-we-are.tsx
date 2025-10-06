2fd969a117506cdf480ac352fb2aa7cbe8f7cd60
Divya Chachane <divyachachane06@gmail.com>
2025-10-06 07:15:47 +0000

    update

--- src/components/who-we-are.tsx
+++ src/components/who-we-are.tsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 
 export default function WhoWeAre() {
   return (
-    <section className="about-area pt-16 pb-16 bg-blue-950 text-white">
+    <section className="about-area pt-16 pb-16  text-black">
       <div className="container mx-auto max-w-6xl px-6">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
           {/* Left Image */}
           <div className="order-last lg:order-first">
             <Image
-              src="/assests/images/about.png"
+              src="/assests/images/about-3.png"
               alt="About Us"
               width={600}
               height={600}
@@ -25,15 +25,15 @@ export default function WhoWeAre() {
             <h5 className="text-primary font-bold uppercase tracking-wide">
               WHO WE ARE
             </h5>
-            <h2 className="text-3xl font-extrabold text-white mt-2">
+            <h2 className="text-3xl font-extrabold text-black mt-2">
               Empowering Ethical AI Innovation, Together
             </h2>
             <div className="em_bar_bg text-left mt-4 mb-6"></div>
-            <p className="text-base text-gray-300 mb-6">
+            <p className="text-base text-black mb-6">
               AI Innovation Society is a global platform fostering ethical,
               inclusive, and impactful advancements in Artificial Intelligence.
             </p>
-            <p className="text-base text-gray-300">
+            <p className="text-base text-black">
               We empower students, researchers, and professionals by providing
               access to high-quality education, supporting open-access
               research, and building a collaborative ecosystem for responsible
